453f11abdb282cf511d8b827442a870c4ba408fa
RizeleV <rav8501@students.uc-bcf.edu.ph>
2026-04-18 22:33:50 +0800

    Added necessary adjustments for personalization

--- Contact.jsx
+++ Contact.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
 function Contact() {
-   return (
-    <section className="my-12">
-      <h2 className="text-3xl font-semibold text-blue-600">Contact</h2>
-      <p className="text-lg mt-4">
-       This is contact section
+  return (
+    <section id="contact" className="scroll-mt-24 bg-white text-black p-10 rounded-2xl shadow-md"
+    >
+      <h2 className="text-3xl font-bold mb-4">Send me a Message:</h2>
+
+      <p className="mb-6 text-gray-600">
+        I’d love to hear from you. Drop a message below!
       </p>
+
+      {/* TEXTAREA */}
+      <textarea
+        placeholder="Write your message here..."
+        className="w-full h-40 p-4 rounded-lg bg-gray-100 border-2 border-[#6b0e1d] text-black outline-none resize-none focus:ring-2 focus:ring-[#6b0e1d]"
+      ></textarea>
+
+      {/* BUTTON ROW */}
+      <div className="flex justify-end mt-4">
+        <button className="bg-[#6b0e1d] text-white px-6 py-2 rounded-lg font-semibold hover:bg-red-800 transition">
+          Send
+        </button>
+      </div>
     </section>
   );
 }
 
-export default Contact
+export default Contact;
--- Experience.jsx
+++ Experience.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 
 function Experience() {
   return (
-    <section className="my-12">
-      <h2 className="text-3xl font-semibold text-blue-600">Experience</h2>
-      <p className="text-lg mt-4">
-       Experience Section
-      </p>
+    <section id="experience" className="bg-white p-10 scroll-mt-24 rounded-2xl shadow-md hover:shadow-xl transition">
+      <h2 className="text-3xl font-bold text-[#6b0e1d] mb-6 border-b-2 pb-2 inline-block">
+        Experience
+      </h2>
+
+      <div className="grid md:grid-cols-2 gap-6">
+        <div className="p-5 border rounded-xl hover:bg-gray-50 transition">
+          <h3 className="font-semibold text-lg">Customer Representative</h3>
+          <p className="text-gray-600 text-sm">Nov 2023 - Feb 2024</p>
+          <p className="mt-2 text-gray-700 text-sm">
+            Assisted customers, handled inquiries, and provided admin support.
+          </p>
+        </div>
+
+        <div className="p-5 border rounded-xl hover:bg-gray-50 transition">
+          <h3 className="font-semibold text-lg">Visual Web Novel</h3>
+          <p className="text-gray-600 text-sm">Academic Project</p>
+          <p className="mt-2 text-gray-700 text-sm">
+           Lead Programmer using Monogatari engine, story board director and art illustrator.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
